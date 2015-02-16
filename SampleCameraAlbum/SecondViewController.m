3348f49edb0b2675252ff8973d7587859a75eb0f
ei17ringo <ei17ringo@gmail.com>
2015-02-16 14:21:53 +0800

    写真配置（テスト）

--- SampleCameraAlbum/SecondViewController.m
+++ SampleCameraAlbum/SecondViewController.m
@@ -18,16 +18,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     UIImage *image = [UIImage imageNamed:@"nopreview.png"];
-    UIImageView *imagev = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 160, 160)];
-    imagev.image = image;
-    
-    [self.view addSubview:imagev];
     
     
-    UIImageView *imagev2 = [[UIImageView alloc]initWithFrame:CGRectMake(160, 0, 160, 160)];
-    imagev2.image = image;
+    for (int i=0; i < 30; i++) {
+        int x = 0;
+        int y = 0;
+        
+        if (i % 2 == 1) {
+            x = 160;
+        }
+        
+        y = 160 * (i / 2);
+        
+        
+        UIImageView *imagev = [[UIImageView alloc]initWithFrame:CGRectMake(x, y, 160, 160)];
+        imagev.image = image;
+        
+        [self.view addSubview:imagev];
+        
+    }
     
-    [self.view addSubview:imagev2];
+    
 }
 
 - (void)didReceiveMemoryWarning {
